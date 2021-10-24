76664877216561eef301ca2bc534de3e3e009945
Englert Ervin  Brunó <englert@users.noreply.github.com>
2021-10-24 16:05:57 +0000

    Statisztika LINQ-val

--- main.cs
+++ main.cs
@@ -7,12 +7,21 @@ using System.Text;                // Encoding
 using System.Collections.Generic; // List<>, Dictionary<>
 using System.Linq;                // from where select
 
-class Dolgozo{                    // "Minimalista" osztály definíció
-	public string nev;
-	public string neme;
-	public string reszleg;
-	public string belepes;
-	public int ber;
+class Dolgozo{
+	public string nev       {get; set;}
+	public string neme      {get; set;}
+	public string reszleg   {get; set;}
+	public string belepes   {get; set;}
+	public int ber          {get; set;}
+
+    public Dolgozo(string sor){
+        var s = sor.Trim().Split(';');
+        this.nev     = s[0]; 
+        this.neme    = s[1]; 
+        this.reszleg = s[2]; 
+        this.belepes = s[3]; 
+        this.ber     = int.Parse(s[4]); 
+    }
 }
 
 class Program {
@@ -23,14 +32,8 @@ class Program {
         var elsosor = f.ReadLine();                   // az elsosorba kerül a fájl első sora.
         
         while (!f.EndOfStream){              // amig a fájl végére nem érünk addig imétlünk
-		  var dolgozo = new Dolgozo();              // létrehozunk egy dolgozo-t a Dolgozó osztály alapján
-		  var s = f.ReadLine().Split(';');          // az s változóba kerül a beolvasott és a pontosvessző mentén feldarabolt sor       
-		  dolgozo.nev = s[0];                       // a dolgozo.nev-be      kerül az s lista nulladik eleme
-		  dolgozo.neme = s[1];                      // a dolgozo.neme-be     kerül az s lista nulladik eleme
-		  dolgozo.reszleg = s[2];                   // a dolgozo.reszleg-be  kerül az s lista nulladik eleme
-		  dolgozo.belepes = s[3];                   // a dolgozo.belepes-be  kerül az s lista nulladik eleme
-		  dolgozo.ber = int.Parse(s[4]);            // a dolgozo.nev -be     kerül az s lista nulladik eleme egész számmá alaítva
-		  lista.Add(dolgozo);                       // a lista-hoz adjuk a dolgozo példányt.
+		  var dolgozo = new Dolgozo(f.ReadLine());              // létrehozunk egy dolgozo-t a Dolgozó osztály alapján
+		  lista.Add(dolgozo);                // a lista-hoz adjuk a dolgozo példányt.
 		}
         f.Close();                           // bezárjuk az f fájlt
 
@@ -46,10 +49,9 @@ class Program {
         var reszlegneve = Console.ReadLine();
 
 // 6. feladat: A legtöbbet kereső dolgozó a megadott részlegen
-        var res = (from sor in lista where (sor.reszleg == reszlegneve) select (sor.ber, sor));	// a from végigpörgeti a lista-t, 
-        // az egyes sorok a sor- ba kerülnek a where kiválaszja a feltételben megfelelőket, ezeket a  select "kitolja" mint (sor.ber, sor) a res-be 
+        var res = (from sor in lista where (sor.reszleg == reszlegneve) orderby sor.ber select sor);	
         if (res.Count()>0){
-            var maxi = res.Max().Item2; // A Max() kiválastja a res-ben levő sorok első adata alapján (bér) a legnagyobbat, a második adat (sor) kerül a maxi-ba
+            var maxi = res.Last();
             Console.WriteLine($"6. feladat: A legtöbbet kereső dolgozó a megadott részlegen");
             Console.WriteLine($"        Név: {maxi.nev}");
             Console.WriteLine($"        Neme: {maxi.neme}");
@@ -60,20 +62,12 @@ class Program {
             Console.WriteLine($"6. feladat: A megadott részleg nem létezik a cégnél!");
         }
         
-         //7. feladat: Statisztika
-        var statisztika = new Dictionary<string, int>(); // Üres szótár létrehozása statisztika néven
-        foreach (var sor in lista) {                     // a foreach végiggyalogol a lista-n, a lista sorai a sor-ba kerulnek
-            if (statisztika.ContainsKey(sor.reszleg)) {  // ha a sor-ban levő részleg mező mint kulcs benne van a statisztika szótárban
-                statisztika[sor.reszleg]++;              // akkor a kulcshoz tartozó értéket megnöveli eggyel
-            } 
-            else {                                       // egyébként
-            statisztika[sor.reszleg] = 1;                // a statisztika szótárban létrehozza a sor.reszleg változó alapján az új kulcsot, 
-            }                                            // amihez az 1 egész számot rendeli értkként.
+        //7. feladat: Statisztika
+        Console.WriteLine($"7. feladat: Statisztika"); 
+        var statisztika = (from sor in lista group sor by sor.reszleg);
+        foreach( var q in statisztika ){
+            Console.WriteLine($"        {q.Key}: {q.Count()} ");
         }
-        Console.WriteLine($"7. feladat: Statisztika");
-        foreach(var kulcs_ertek in statisztika){         // a foreach végiggyalogol a statisztika-n, a kulcs - érték párok a kulcs_ertek- be kerülnek
-            Console.WriteLine($"        {kulcs_ertek.Key} - {kulcs_ertek.Value} ");
-        }
-       
+//  -----------------------------------------------------------------------
     }  
 }
